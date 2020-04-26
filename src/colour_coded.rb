f6b9a98d094b7b1839fff1063688fa3de2767179
Thomas Fisher <thomasjfisher1@gmail.com>
2020-04-26 13:22:13 +1000

    Uploading Flow Chart and almost finialised code. Few errors to iron out.

--- src/colour_coded.rb
+++ src/colour_coded.rb
@@ -13,24 +13,25 @@ def clear_terminal
 	
 end
   
-clear_terminal
-puts "Welcome"
-sleep 1
-clear_terminal
-puts "To"
-sleep 1
-clear_terminal
+# clear_terminal
+# puts "Welcome"
+# sleep 1
+# clear_terminal
+# puts "To"
+# sleep 1
+# clear_terminal
 puts "
 	____ ____ ____ ____ ____ ____ _________ ____ ____ ____ ____ ____ 
 	||C |||O |||L |||O |||U |||R |||       |||C |||O |||D |||E |||D ||
 	||__|||__|||__|||__|||__|||__|||_______|||__|||__|||__|||__|||__||
 	|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/_______\\|/__\\|/__\\|/__\\|/__\\|/__\\|"
-sleep 3
+# sleep 3
 puts
 puts "Would you like to play? [y/n]"
 user_play = gets.chomp.downcase
 	while user_play != 'y'	&& user_play != 'n'
-	puts "Invlaid command, Would you like to play? [y/n]"
+	puts "Invlaid command."
+	puts "Would you like to play? [y/n]"
 	user_play = gets.chomp.downcase
 	end
 if user_play != 'y'
@@ -46,15 +47,20 @@ def clear_terminal
 	Gem.win_platform? ? (system "cls") : (system "clear")
 end
 
-	while counter < 5 
+	while counter < 5
     	word = words.shuffle 
 		color = colors.sample  
 		word = word[0..counter].join(" ")
 		
 		if word != color 
-			puts "The word to remember is ..."
+			clear_terminal
+			puts "Alright, now, all you have to do is..."
+			sleep 1			
+			puts			
+			puts "Remember to remember..."
 			sleep 2
 			clear_terminal
+			sleep 1
 			print word.colorize(color) + " "
 			
 			sleep 1
@@ -63,8 +69,9 @@ end
 			user_input = gets.chomp.downcase
 
 			if word == user_input 
-				puts "Well done! Keep it up!"
-				sleep 1
+				grats = ["Hmm, have you done this before?",	"Hey! No peeking! Oh wait, no... I guess thats allowed...", "beep beep bop bo boop", "Woah, how'd you get that?", "nice", "Oh, and here I thought it was: up up down zero zero back?", "Still going hey?"].shuffle
+				puts grats[0]
+				sleep 3
 				clear_terminal
                 counter += 1
 				wrong_answer = 0
@@ -79,31 +86,45 @@ end
                 
 				wrong_answer +=1
 				guess_count +=1
+			
+
                 if wrong_answer == 3
 					puts "Game Over " * 550
+					sleep 5
+					counter = 0
 				
-					if counter >= 5
+				elsif counter >= 5
 					puts "You Won! " * 550
+				
 					
 					sleep 2
+					puts
+				
 					puts "Would you like ot play again? [y/n]"
-					# clear_terminal
-					# if wrong_answer == 3 && counter == 5
-					# puts "Would you like to play again? [y/n]"
-					# play_again = gets.chomp.downcase
-					# 	if play_again == 'y'
-					# 		guess_count = 1
-					# 		wrong_answer = 0
-					# 		counter = 0
-					# 		next
-					# 	elsif
-					# 		play_again == 'n'
-					# 		puts "Goodbye!"
+					puts
+					play_again = gets.chomp.downcase
+						while play_again != 'y'	&& play_again != 'n'
+						puts "Invlaid command."
+						puts "Would you like to play again? [y/n]"
+						user_play = gets.chomp.downcase
+						
+						if play_again != 'y'
+							puts "Thanks for playing"
+							sleep 1
+							puts
+							puts "
+							____ ____ ____ ____ ____ ____ _________ ____ ____ ____ ____ ____ 
+							||C |||O |||L |||O |||U |||R |||       |||C |||O |||D |||E |||D ||
+							||__|||__|||__|||__|||__|||__|||_______|||__|||__|||__|||__|||__||
+							|/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/_______\\|/__\\|/__\\|/__\\|/__\\|/__\\|"
+							sleep 3
+							exit
+						end
 					end
-						end	
-					end
-                end
+				end	
 			end
+		end 
+	end
 		
 	
 			
--- src/colour_coded_main_menu.rb
+++ src/colour_coded_main_menu.rb
@@ -1,28 +1,36 @@
 require 'colorize'
 require 'colorized_string'
 require 'tty-prompt'
+require 'tty-cursor'
+require 'lolize/auto'
+
+cursor = TTY::Cursor
 
 def clear_terminal
 	Gem.win_platform? ? (system "cls") : (system "clear")
 	
 end
 
-clear_terminal
-puts "Welcome"
-sleep 1
-clear_terminal
-puts "To"
-sleep 1
-clear_terminal
-puts "Colour Coded"
-sleep 2
-clear_terminal
-sleep 1
+
+
+# clear_terminal
+# puts "Welcome"
+# sleep 1
+# clear_terminal
+# puts "To"
+# sleep 1
+# clear_terminal
+# puts "Colour Coded"
+# sleep 2
+# clear_terminal
+# sleep 1
+
 
 user_main_menu_select = ""
 while user_main_menu_select !=3
-
-    puts "Would you like to do?"
+    
+    
+    puts "What would you like to do?"
     sleep 1
     puts
     puts "1. Play Game"
@@ -30,9 +38,10 @@ while user_main_menu_select !=3
     puts "2. Read Rules"
     puts
     puts "3. Quit"
-    sleep 1
+    puts 
     puts "Please enter your selection: [1,2,3]"
-    
+    puts 
+   
     user_main_menu_select = gets.to_i
     sleep 1
 
@@ -40,10 +49,44 @@ while user_main_menu_select !=3
         puts "GET READY!"
         sleep 1
     elsif user_main_menu_select == 2
-        puts "Rules"
+       clear_terminal
+        puts "Welcome to"
         sleep 1
-        puts "Welcome to Colour Coded.\nRules are:"
+        puts  "
+        ____ ____ ____ ____ ____ ____ _________ ____ ____ ____ ____ ____ 
+        ||C |||O |||L |||O |||U |||R |||       |||C |||O |||D |||E |||D ||
+        ||__|||__|||__|||__|||__|||__|||_______|||__|||__|||__|||__|||__||
+        |/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/_______\\|/__\\|/__\\|/__\\|/__\\|/__\\|"
+        puts
+        sleep 1
+        puts "Here are the rules!"
+        puts 
+        sleep 1
+        puts "The aim of the game is to remember the sequence of words displayed across your screen"
+        sleep 3
+        puts
+        puts "You have 3 attempts to input your guess correctly"
+        sleep 3
+        puts
+        puts "If you unfortunately do not guess the sequence correctly, after 3 wrong guesses"
+        sleep 3
+        puts
+        puts "the game will end, and you will be asked if you would like to play again."
+        sleep 10
+        clear_terminal
+        
     elsif user_main_menu_select == 3
+        puts "Goodbye from"
+        sleep 1
+        puts "
+        ____ ____ ____ ____ ____ ____ _________ ____ ____ ____ ____ ____ 
+        ||C |||O |||L |||O |||U |||R |||       |||C |||O |||D |||E |||D ||
+        ||__|||__|||__|||__|||__|||__|||_______|||__|||__|||__|||__|||__||
+        |/__\\|/__\\|/__\\|/__\\|/__\\|/__\\|/_______\\|/__\\|/__\\|/__\\|/__\\|/__\\|"
+        sleep 1
+        puts 
+        puts "We hope to see you soon!"
+        sleep 1
         exit
     else
         clear_terminal
